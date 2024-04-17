1d480ce2d454996e2a75db97b216b526a52847cf
Sabasiva2916 <sabasiva2916@gmail.com>
2024-04-17 11:56:26 +0530

    reverse programm

--- src/org/sample/SampleClass.java
+++ src/org/sample/SampleClass.java
@@ -4,5 +4,13 @@ public class SampleClass {
 
     public static void main(String[] args) {
 	System.out.println("Hello World!!!");
+	// Reverse of String
+	String str = "java programm";
+	String output = "";
+	for (int i = str.length() - 1; i >= 0; i--) {
+	    char c = str.charAt(i);
+	    output = output + c;
+	}
+	System.out.println("Reverse of String :" + output);
     }
 }
