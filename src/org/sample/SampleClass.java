11efc24b07b6637de5820daaf0fa23a490134068
Sabasiva2916 <sabasiva2916@gmail.com>
2024-04-17 12:11:33 +0530

    occurance Code

--- src/org/sample/SampleClass.java
+++ src/org/sample/SampleClass.java
@@ -1,8 +1,26 @@
 package org.sample;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class SampleClass {
 
     public static void main(String[] args) {
 	System.out.println("Hello World!!!");
+	// occurance
+	String str = "java programm";
+	Map<Character, Integer> n = new LinkedHashMap<>();
+	for (int i = 0; i < str.length(); i++) {
+	    char c = str.charAt(i);
+	    if (n.containsValue(n.get(c))) {
+		Integer integer = n.get(c);
+		n.put(c, integer + 1);
+	    } else {
+		n.put(c, 1);
+	    }
+
+	}
+
+	System.out.println(n);
     }
 }
